38ef178fc0153adff46aa6ad41c11de9308340cf
banderGlitch <nernaykumar98@gmail.com>
2025-03-23 16:10:02 +0530

    changes

--- eventmanagement/src/components/layout/Navbar/Navbar.jsx
+++ eventmanagement/src/components/layout/Navbar/Navbar.jsx
@@ -37,3 +37,43 @@ const Navbar = () => {
 };
 
 export default Navbar; 
+
+
+// function subNewArr(arr, target) {
+//     let left = 0; right = arr.length - 1
+//     let mid = Math.floor((left + right) / 2)
+//     console.log("mid", mid)
+//     if (arr[mid] === target) {
+//         return mid // if target element is middle then no problem
+//     } else {
+//         while (arr[left] < arr[mid]) {
+//             mid = mid + 1
+//         }
+
+//         left = mid; right = arr.length - 1
+//         while (left < right) {
+//             console.log("this is called!!")
+//             let sub_mid = Math.floor((right + left) / 2)
+//             if (arr[sub_mid] === target) {
+//                 return sub_mid
+//             } else if (arr[sub_mid] < target) {
+//                 left = sub_mid + 1
+//             } else {
+//                 right = sub_mid - 1
+//             }
+//         }
+//         left = 0; right = mid - 1
+//         while (left < right) {
+//             console.log("this is called!!")
+//             let sub_mid = Math.floor((right + left) / 2)
+//             if (arr[sub_mid] === target) {
+//                 return sub_mid
+//             } else if (arr[sub_mid] < target) {
+//                 left = sub_mid + 1
+//             } else {
+//                 right = sub_mid - 1
+//             }
+//         }
+//         return -1
+//     }
+// }
